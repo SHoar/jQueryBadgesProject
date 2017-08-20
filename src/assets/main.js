972d522cfaf6c781ae56b2902589a675ffc5faea
Sean Hoar <sean.hoar@gmail.com>
2017-08-20 13:39:29 -0400

    Add boilerplate ajax call, start adjusting

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
-
+  var URL = 'https://www.codeschool.com/users/SHoar.json';
+  $.ajax({
+    url: URL,
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      $.each(response, function(index, badge) {
+        var badge = $('#badges');
+        badge.find('p').html( courses.completed.title );
+        badge.find('img')
+             .attr('src', courses.completed.badge);
+        badge.find('a').attr('href', courses.completed.url);
+      });
+    }
+  });
 });
